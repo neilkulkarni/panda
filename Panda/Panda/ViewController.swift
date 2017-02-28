9f7a0b07c8ad974c2632318ba843b6df3afa8fd1
Winnie <winnielin@Winnies-MacBook-Pro.local>
2017-02-28 14:32:55 -0500

    Panda on xcode

---
+++ Panda/Panda/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Panda
+//
+//  Created by Winnie Lin on 2/28/17.
+//  Copyright © 2017 Panda. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
