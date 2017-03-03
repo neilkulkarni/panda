a6e58885717bbf23fb850ba4621319fbff11ded4
Neil Kulkarni <neil_kulkarni@icloud.com>
2017-03-03 11:56:30 -0500

    added UI for create an account
    checks name, email, password, and confirmation for appropriate usage

---
+++ Panda_Thurs/Panda/Panda/Panda/CreateAccountViewController.swift
@@ -0,0 +1,158 @@
+//
+//  CreateAccountViewController.swift
+//  Panda
+//
+//  Created by Neil Kulkarni on 3/3/17.
+//  Copyright © 2017 Panda. All rights reserved.
+//
+
+import Cocoa
+
+class CreateAccountViewController: NSViewController {
+
+    
+    @IBOutlet weak var nameField: NSTextField!
+    @IBOutlet weak var nameErrorLabel: NSTextField!
+    
+    @IBOutlet weak var emailField: NSTextField!
+    @IBOutlet weak var emailErrorLabel: NSTextField!
+    
+    @IBOutlet weak var passwordField: NSSecureTextField!
+    @IBOutlet weak var passwordErrorLabel1: NSTextField!
+    @IBOutlet weak var passwordErrorLabel2: NSTextField!
+    @IBOutlet weak var passwordErrorLabel3: NSTextField!
+    
+    @IBOutlet weak var confirmPasswordField: NSSecureTextField!
+    @IBOutlet weak var confirmPasswordErrorLabel: NSTextField!
+    
+    @IBOutlet weak var termsCheckbox: NSButton!
+    @IBOutlet weak var termsErrorLabel: NSTextField!
+    
+    
+    @IBOutlet weak var createAccountButton: NSButton!
+    @IBOutlet weak var createAccountErrorLabel: NSTextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    var name:String?
+    
+    var email:String?
+    
+    var password:String?
+    var confirmPassword:String?
+    
+    var nameErrorFlag:Bool = true;
+    var emailErrorFlag:Bool = true;
+    var passwordErrorFlag:Bool = true;
+    var confirmPasswordErrorFlag:Bool = true;
+    var termsErrorFlag:Bool = true;
+    
+    
+    @IBAction func exitName(_ sender: Any) {
+        name = trim(text: nameField.stringValue)
+        
+        if (matches (for: "[a-zA-z]+([ '-][a-zA-Z]+)*", in: name!).count == 0) {
+            nameErrorLabel.isHidden = false;
+            nameErrorFlag = true;
+        }
+        else {
+            nameErrorLabel.isHidden = true;
+            nameErrorFlag = false;
+            
+            enableCreateAccountButton()
+        }
+    }
+    
+    @IBAction func exitEmail(_ sender: Any) {
+        email = trim(text: emailField.stringValue)
+        
+        if (matches(for: "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$", in: email!).count == 0) {
+            emailErrorLabel.isHidden = false;
+            emailErrorFlag = true;
+        }
+        else {
+            emailErrorLabel.isHidden = true;
+            emailErrorFlag = false;
+        }
+        
+        enableCreateAccountButton()
+    }
+    
+    @IBAction func exitPassword(_ sender: Any) {
+        password = trim(text: passwordField.stringValue)
+        
+        if (matches(for: "^([a-zA-Z0-9]{8,20})$", in: password!).count == 0) {
+            passwordErrorLabel1.isHidden = false;
+            passwordErrorLabel2.isHidden = false;
+            passwordErrorLabel3.isHidden = false;
+            passwordErrorFlag = true;
+        }
+        else {
+            passwordErrorLabel1.isHidden = true;
+            passwordErrorLabel2.isHidden = true;
+            passwordErrorLabel3.isHidden = true;
+            passwordErrorFlag = false;
+        }
+        
+        enableCreateAccountButton()
+    }
+    
+    @IBAction func exitConfirmPassword(_ sender: Any) {
+        confirmPassword = trim(text: confirmPasswordField.stringValue)
+        
+        if (password != confirmPassword) {
+            confirmPasswordErrorLabel.isHidden = false;
+            confirmPasswordErrorFlag = true;
+        }
+        else {
+            confirmPasswordErrorLabel.isHidden = true;
+            confirmPasswordErrorFlag = false;
+        }
+        
+        enableCreateAccountButton()
+    }
+    
+    @IBAction func termsSelected(_ sender: Any) {
+        if (termsCheckbox.state == NSOffState) {
+            termsErrorLabel.isHidden = false;
+            termsErrorFlag = true;
+        }
+        else {
+            termsErrorLabel.isHidden = true;
+            termsErrorFlag = false;
+        }
+        
+        enableCreateAccountButton()
+    }
+    
+    
+    func trim(text:String) -> String {
+        return text.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+    
+    func matches(for regex: String, in text: String) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let nsString = text as NSString
+            let results = regex.matches(in: text, range: NSRange(location: 0, length: nsString.length))
+            return results.map { nsString.substring(with: $0.range)}
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    func enableCreateAccountButton() {
+        if (nameErrorFlag || emailErrorFlag || passwordErrorFlag || confirmPasswordErrorFlag || termsErrorFlag) {
+            createAccountButton.isEnabled = false;
+        }
+        else {
+            createAccountButton.isEnabled = true;
+        }
+    }
+}
