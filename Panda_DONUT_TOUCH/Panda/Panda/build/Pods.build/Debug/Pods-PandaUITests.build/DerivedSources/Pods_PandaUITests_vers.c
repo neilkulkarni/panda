982bf2878cbe9578eb5040421cf24c85fef6af81
Neil Kulkarni <neil_kulkarni@icloud.com>
2017-03-08 12:31:25 -0500

    duplicated sprint 1 progress

---
+++ Panda_DONUT_TOUCH/Panda/Panda/build/Pods.build/Debug/Pods-PandaUITests.build/DerivedSources/Pods_PandaUITests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_PandaUITestsVersionString[];
+ extern const double Pods_PandaUITestsVersionNumber;
+
+ const unsigned char Pods_PandaUITestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_PandaUITests  PROJECT:Pods-1" "\n";
+ const double Pods_PandaUITestsVersionNumber __attribute__ ((used)) = (double)1.;
