c72cdf0639234ebb36400929564a1a6bec864947
Neil Kulkarni <neil_kulkarni@icloud.com>
2017-04-17 04:05:05 -0400

    APIs for Interacting with Trips and Events
    POST http://localhost:8081/trip
    - creates a new trip
    
    GET http://localhost:8081/trip/{id}
    - get a trip with id
    
    GET http://localhost:8081/all-trips/{id}
    - get all trips for user with id
    
    GET http://localhost:8081/public-trips/{id}
    - get public trips for user with id
    
    PUT http://localhost:8081/trip
    - update trip
    
    POST http://localhost:8081/event
    - creates a new event
    
    GET http://localhost:8081/event/{id}
    - get a event with id
    
    GET http://localhost:8081/events/{id}
    - get all events for trip with id
    
    PUT http://localhost:8081/event
    - update event

--- server/script.js
+++ server/script.js
@@ -169,6 +169,226 @@ router.get('/login', function(request, response) {
     });
 });
 
+// route create a user (accessed at POST http://localhost:8081/users)
+router.post('/trip', function(request, response) {
+    var q = request.body; 
+    var trip = {
+        name: q.name, 
+        description: q.description,
+        private: q.private,
+        api: q.api,
+        user_id: q.user_id
+    } 
+    
+    conn.query('INSERT INTO trip SET ?', trip, function(err, result) {
+        console.log('Added trip to user ?', trip.user_id);
+
+        var code = { code: 200 };
+        response.send(code);
+        
+        console.log(trip);
+    });
+});
+
+// route get all users (accessed at GET http://localhost:8081/users)
+router.get('/all-trips/:id', function(request, response) {        
+    var userID = request.params.id;
+    var tripList = [];
+    conn.query('SELECT * FROM trip WHERE user_id=?', [userID], function(err, result) {
+        for (var i = 0; i < result.length; i++) {
+            var tempTrip = { 
+                id: result[i].id, 
+                name: result[i].name, 
+                description: result[i].description,
+                private: result[i].private,
+                api: result[i].api,
+                user_id: result[i].user_id
+            };
+        
+            tripList.push(tempTrip);
+        }
+        
+        response.json({ tripList });
+    });
+});
+
+// route get all users (accessed at GET http://localhost:8081/users)
+router.get('/public-trips/:id', function(request, response) {        
+    var userID = request.params.id;
+    var tripList = [];
+    conn.query('SELECT * FROM trip WHERE user_id=? && private=0', [userID], function(err, result) {
+        for (var i = 0; i < result.length; i++) {
+            var tempTrip = { 
+                id: result[i].id, 
+                name: result[i].name, 
+                description: result[i].description,
+                private: result[i].private,
+                api: result[i].api,
+                user_id: result[i].user_id
+            };
+        
+            tripList.push(tempTrip);
+        }
+        
+        response.json({ tripList });
+    });
+});
+
+// route get a user (accessed at GET http://localhost:8081/user/{id})
+router.get('/trip/:id', function(request, response) {
+    var tripID = request.params.id;
+    var trip;
+    conn.query('SELECT * FROM trip WHERE id=?', [tripID], function(err, result) {
+        trip = {
+            id: result[0].id, 
+            name: result[0].name, 
+            description: result[0].description,
+            private: result[0].private,
+            api: result[0].api,
+            user_id: result[0].user_id
+        };
+        
+        response.json({ trip });
+    });
+});
+
+
+router.put('/trip', function(request, response) {
+    var q = request.body;
+    var id = q.id;
+    var description = q.description;
+    var private = q.private;
+    var api = q.api;
+    
+    console.log(q);
+    
+    conn.query('SELECT * FROM trip', function(err, result) {
+        var error = 1;
+        for (var i = 0; i < result.length; i++) {
+            if (result[i].id == q.id) {
+                console.log('Trip exists.');
+
+                conn.query('UPDATE trip SET description=?, private=?, api=? WHERE id=?', 
+                    [description, private, api, id], function(err, result) {
+                    var code = { code: 200 };   
+                    response.send(code); 
+                });
+                
+                error = 0;
+                break;
+            }
+        }
+        
+        if (error == 1) {    
+                var code = { code: 400 };   
+                response.send(code); 
+        }
+    });
+});
+
+// route create a user (accessed at POST http://localhost:8081/users)
+router.post('/event', function(request, response) {
+    var q = request.body; 
+    var event = {
+        name: q.name, 
+        description: q.description,
+        latitude: q.latitude,
+        longitude: q.longitude,
+        date: q.date,
+        api: q.api,
+        trip_id: q.trip_id
+    } 
+    
+    conn.query('INSERT INTO event SET ?', event, function(err, result) {
+        console.log('Added trip to event ?', event.trip_id);
+
+        var code = { code: 200 };
+        response.send(code);
+        
+        console.log(event);
+    });
+});
+
+// route get all users (accessed at GET http://localhost:8081/users)
+router.get('/events/:id', function(request, response) {        
+    var tripID = request.params.id;
+    var eventList = [];
+    conn.query('SELECT * FROM event WHERE trip_id=?', [tripID], function(err, result) {
+        for (var i = 0; i < result.length; i++) {
+            var tempEvent = { 
+                id: result[i].id, 
+                name: result[i].name, 
+                description: result[i].description,
+                latitude: result[i].latitude,
+                longitude: result[i].longitude,
+                date: result[i].date,
+                api: result[i].api,
+                trip_id: result[i].trip_id
+            };
+        
+            eventList.push(tempEvent);
+        }
+        
+        response.json({ eventList });
+    });
+});
+
+// route get a user (accessed at GET http://localhost:8081/user/{id})
+router.get('/event/:id', function(request, response) {
+    var eventID = request.params.id;
+    var event;
+    conn.query('SELECT * FROM event WHERE id=?', [eventID], function(err, result) {
+        event = {
+            id: result[0].id, 
+            name: result[0].name, 
+            description: result[0].description,
+            latitude: result[0].latitude,
+            longitude: result[0].longitude,
+            date: result[0].date,
+            api: result[0].api,
+            trip_id: result[0].trip_id
+        };
+        
+        response.json({ event });
+    });
+});
+
+router.put('/event', function(request, response) {
+    var q = request.body;
+    var id = q.id;
+    var description = q.description;
+    var latitude = q.latitude;
+    var longitude = q.longitude;
+    var date = q.date;
+    var api = q.api;
+    
+    console.log(q);
+    
+    conn.query('SELECT * FROM event', function(err, result) {
+        var error = 1;
+        for (var i = 0; i < result.length; i++) {
+            if (result[i].id == q.id) {
+                console.log('Event exists.');
+
+                conn.query('UPDATE event SET description=?, latitude=?, longitude=?, date=?, api=? WHERE id=?', 
+                    [description, latitude, longitude, date, api, id], function(err, result) {
+                    var code = { code: 200 };   
+                    response.send(code); 
+                });
+                
+                error = 0;
+                break;
+            }
+        }
+        
+        if (error == 1) {    
+                var code = { code: 400 };   
+                response.send(code); 
+        }
+    });
+});
+
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be not be prefixed
 app.use('', router);
