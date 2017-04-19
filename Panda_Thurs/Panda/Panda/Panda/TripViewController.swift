71e9d7e48fd6ec2cd8b98cd30fe9ca251782d93b
Winnie <winnielin@pal-nat186-60-86.itap.purdue.edu>
2017-04-19 15:48:15 -0400

    add some graphics
    -added graphics to decorate ui

--- Panda_Thurs/Panda/Panda/Panda/TripViewController.swift
+++ Panda_Thurs/Panda/Panda/Panda/TripViewController.swift
@@ -64,7 +64,12 @@ class TripViewController: NSViewController {
             //profilePictureView.image = image
             pictureUpperLeft.image = image;
           //  user.setPicture(picture: imageChosen!.absoluteString)
+          /*  if let bits = image?.representations.first as? NSBitmapImageRep {
+                let data = bits.representationUsingType(using: NSJPEGFileType, properties: [:])
+               // data?.writeToFile("/path/myImage.jpg", atomically: false)
+            }*/
             
+  
         }
         if(pictureUpperLeft != nil){
             addButtonUpperLeft.isHidden = true;
